6b50d8097bcdf7b0f7d96acccfe79c56d439f3b1
krisskross <kristoffer.kleven.krossholm@schibsted.com>
2023-04-26 12:57:21 +0200

    initial commit spring kotlin gradle project

---
+++ src/main/kotlin/com/krossholm/smarthome/SmarthomeApplication.kt
@@ -0,0 +1,11 @@
+package com.krossholm.smarthome
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmarthomeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmarthomeApplication>(*args)
+}
